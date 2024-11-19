2dc973ba0809735648f465016899ebb628743c32
huyendangthithanh <huyen.dangthithanh@ncc.asia>
2024-11-19 15:17:47 +0700

    message

--- src/pageObjects/components/dropdown.ts
+++ src/pageObjects/components/dropdown.ts
@@ -4,7 +4,7 @@ import { BaseComponent } from "../base.component";
 export default class Dropdown extends BaseComponent {
   async verifyDropdownOptions(dropdownLocator: Locator, dataTable: DataTable) {
     const expectedOptions = dataTable.rows().map((row) => row[0]);
-    const actualOptions = (await dropdownLocator.innerText()).split("\n").map((option) => option.trim());
-    expect(actualOptions).toEqual(expectedOptions);
+    const actualOptions = dropdownLocator.locator("option");
+    await expect(actualOptions).toHaveText(expectedOptions);
   }
 }
